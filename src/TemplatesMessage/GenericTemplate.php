3f1e428397a5642a83c76f9750ee42b9481722c8
“Tarciso <tarcisoftorres@g4flex.com.br>
2017-09-06 19:50:06 -0300

    Templates criados

---
+++ src/TemplatesMessage/GenericTemplate.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tarciso
+ * Date: 9/6/17
+ * Time: 11:41 AM
+ */
+
+namespace CodeBot\TemplatesMessage;
+
+use CodeBot\Element\ElementInterface;
+use CodeBot\Message\Message;
+
+class GenericTemplate implements TemplateInterface
+{
+
+    protected $products = [];
+    protected $recipientId;
+
+    public function __construct(string $recipientId)
+    {
+        $this->recipientId = $recipientId;
+    }
+
+    public function add(ElementInterface $element)
+    {
+        $this->products[] = $element->get();
+    }
+
+    public function message(string $messageText) :array
+    {
+        return [
+            'recipient' => [
+                'id' => $this->recipientId
+            ],
+            'message' => [
+                'attachment' => [
+                    'type' => 'template',
+                    'payload' => [
+                        'template_type' => 'generic',
+                        'buttons' => $this->products
+                    ]
+                ]
+            ]
+        ];
+    }
+}
